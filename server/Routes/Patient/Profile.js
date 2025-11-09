27d83cc5b03d923a945d6762e5c1f7667bd60616
KARAN KUMAR <Karanyadav3775@gmail.com>
2025-11-10 02:17:24 +0530

    Fix middleware import path (case-sensitive)

--- server/Routes/Patient/Profile.js
+++ server/Routes/Patient/Profile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const patientProfileController = require('../../Controllers/Patient/Profile');
-const { tokenAuthentication } = require('../../Middleware/User/Authentication');
+const { tokenAuthentication } = require('../../middleware/User/Authentication');
 
 const router = express.Router();
 
